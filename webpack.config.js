cb1e01a5cfc7ffdb82d0e19fd7e49666fd70160b
baruch <lb@LBs-MacBook-Air.home>
2022-01-05 01:59:34 -0500

    typescript using ES6 modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
-const path = require('path')
+const path = require('path');
 
-module.exports={
-    entry: './src/index.ts',
-    module:{
-        rules:[
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                include: [path.resolve(__dirname, 'src')]
-            }
-        ]
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
-    }
-}
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: [path.resolve(__dirname, 'src')],
+        use: 'ts-loader',
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    publicPath: 'public',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+};
